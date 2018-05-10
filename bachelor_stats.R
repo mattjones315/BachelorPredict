eb87588c5f4bd7101982322a3003dc326e3144d1
Christa.Caggiano <christacaggiano@gmail.com>
2018-05-10 16:05:46 -0700

    added comments

--- bachelor_stats.R
+++ bachelor_stats.R
@@ -1,10 +1,47 @@
+# analyzing linear relationships in bachelor data 
+# bmi 209 
+# may 2018 
+# author <christa.caggiano@ucsf.edu> 
 
 
 setwd("~/Documents/UCSF_year1/deep_learning/final_project/")
 rm(list=ls())
 
-library(ggfortify)
+############################### manually one-hot encoding categorical variables ##############################
 
+bachelors = read.csv("bachelors.csv", header=T)
+
+for(unique_value in unique(bachelors$Hometown.Region)){
+  bachelors[paste("Hometown.Region", unique_value, sep = ".")] <- ifelse(bachelors$Hometown.Region == unique_value, 1, 0)
+}
+
+for(unique_value in unique(bachelors$Occupation.Category)){
+  bachelors[paste("Occupation.Category", unique_value, sep = ".")] <- ifelse(bachelors$Occupation.Category == unique_value, 1, 0)
+}
+
+for(unique_value in unique(bachelors$employment.amount)){
+  bachelors[paste("employment.amount", unique_value, sep = ".")] <- ifelse(bachelors$employment.amount == unique_value, 1, 0)
+}
+
+for(unique_value in unique(bachelors$Occupation.Education)){
+  bachelors[paste("Occupation.Education", unique_value, sep = ".")] <- ifelse(bachelors$Occupation.Education == unique_value, 1, 0)
+}
+
+for(unique_value in unique(bachelors$Hair.color)){
+  bachelors[paste("Hair.color", unique_value, sep = ".")] <- ifelse(bachelors$Hair.color == unique_value, 1, 0)
+}
+
+for(unique_value in unique(bachelors$Hair.wavy)){
+  bachelors[paste("Hair.wavy", unique_value, sep = ".")] <- ifelse(bachelors$Hair.wavy == unique_value, 1, 0)
+}
+
+for(unique_value in unique(bachelors$ethnicity)){
+  bachelors[paste("ethnicity", unique_value, sep = ".")] <- ifelse(bachelors$ethnicity == unique_value, 1, 0)
+}
+
+write.csv(bachelors, "bachelor_encoded.csv", row.names = F) # write csv to be used later 
+
+# for contestants 
 b = read.csv("one_hot_encoded.csv", header=T)
 
 for(unique_value in unique(b$Hometown.Region)){
@@ -35,8 +72,10 @@ for(unique_value in unique(b$ethnicity)){
   b[paste("ethnicity", unique_value, sep = ".")] <- ifelse(b$ethnicity == unique_value, 1, 0)
 }
 
-
 encoded = write.csv(b, "encoded.csv", row.names = F)
+
+##################################### simple PCA analysis ############################################
+
 
 row.has.na = apply(encoded, 1, function(x){any(is.na(x))})
 final.encoded = encoded[!row.has.na,]
@@ -47,32 +86,24 @@ final.bach_cat$Age = as.factor(final.bach_cat$Age)
 final.bach_cat$ElimWeek = as.factor(final.bach_cat$ElimWeek)
 final.bach_cat$Season = as.factor(final.bach_cat$Season)
 
-names(final.bach_cat)
 autoplot(prcomp(final.encoded, center=TRUE))
-
 autoplot(prcomp(final.encoded, center=TRUE), data=final.bach_cat, colour="ElimWeek")
 
+##################################### simple cluster analysis ############################################
+
 library(cluster)
-library(lfda)
 
 autoplot(clara(final.encoded, 2))
-
 model <- lfda(final.encoded[-1], final.encoded[,1], r = 2, metric="plain")
 autoplot(model, data = final.bach_cat, frame = TRUE, frame.colour = 'ElimWeek')
 
-barplot(table(final.bach_cat$intro_order, final.bach_cat$ElimWeek))
+##################################### multi-dimensional analysis ############################################
 
 library("RColorBrewer")
 library("gplots")
-heatmap.2(as.matrix(table(final.bach_cat$ElimWeek, final.bach_cat$intro_order)), Rowv=NA)
 
 freq = as.data.frame(table(final.bach_cat$ElimWeek, final.bach_cat$intro_order))
 names(freq) <- c('elim_week', 'intro_order', 'Frequency')
-
-ggplot(freq, aes(elim_week, intro_order)) + geom_tile(aes(fill = Frequency)) + scale_fill_gradient(low="white", high="darkblue")
-
-color.palette = brewer.pal("YlOrRd")
-plot(density())
 
 freq = as.data.frame(table(final.bach_cat$ElimWeek, final.bach_cat$Age))
 names(freq) <- c('elim_week', 'age', 'Frequency')
@@ -83,49 +114,17 @@ final.bach_cat$Age = as.factor(final.bach_cat$Age)
 barplot(table(final.bach_cat$Age))
 
 
-bachelors = read.csv("bachelors.csv", header=T)
-  
-for(unique_value in unique(bachelors$Hometown.Region)){
-  bachelors[paste("Hometown.Region", unique_value, sep = ".")] <- ifelse(bachelors$Hometown.Region == unique_value, 1, 0)
-}
-
-for(unique_value in unique(bachelors$Occupation.Category)){
-  bachelors[paste("Occupation.Category", unique_value, sep = ".")] <- ifelse(bachelors$Occupation.Category == unique_value, 1, 0)
-}
-
-for(unique_value in unique(bachelors$employment.amount)){
-  bachelors[paste("employment.amount", unique_value, sep = ".")] <- ifelse(bachelors$employment.amount == unique_value, 1, 0)
-}
-
-for(unique_value in unique(bachelors$Occupation.Education)){
-  bachelors[paste("Occupation.Education", unique_value, sep = ".")] <- ifelse(bachelors$Occupation.Education == unique_value, 1, 0)
-}
-
-for(unique_value in unique(bachelors$Hair.color)){
-  bachelors[paste("Hair.color", unique_value, sep = ".")] <- ifelse(bachelors$Hair.color == unique_value, 1, 0)
-}
-
-for(unique_value in unique(bachelors$Hair.wavy)){
-  bachelors[paste("Hair.wavy", unique_value, sep = ".")] <- ifelse(bachelors$Hair.wavy == unique_value, 1, 0)
-}
-
-for(unique_value in unique(bachelors$ethnicity)){
-  bachelors[paste("ethnicity", unique_value, sep = ".")] <- ifelse(bachelors$ethnicity == unique_value, 1, 0)
-}
-
-write.csv(bachelors, "bachelor_encoded.csv", row.names = F)
-
-
-rm(list=ls())
+############################# correlations with one-hot encoded  datasets made earlier ####################################
+rm(list=ls()) # start clean 
 
 bach = read.csv("bachelor_encoded.csv", header=T) 
-bach=bach[-3]
+bach=bach[-3]  # remove height column, missing too much data 
 
 cont = read.csv("encoded.csv", header=T)
-cont=cont[,-5]
-cont=cont[,-2]
+cont=cont[,-5] # remove height
+cont=cont[,-2] # season 
 
-
+# make sure all data is numeric 
 for (i in names(bach)){
   bach[[i]] <- as.numeric(bach[[i]])
 }
@@ -134,63 +133,29 @@ for (i in names(cont)){
   cont[[i]] <- as.numeric(cont[[i]])
 }
 
-
+# remove women with missing data 
 row.has.na = apply(cont, 1, function(x){any(is.na(x))})
 cont = cont[!row.has.na,]
 
-season_10_bach = subset(bach, bach$Season==10)
-t(season_10_bach)
-
-cont[1, -1:-2]
-
+# heat map by age
 freq_t = as.data.frame(table(cont$ElimWeek, cont$Age))
 names(freq_t) <- c('elim_week', 'age', 'Frequency')
 ggplot(freq, aes(elim_week, age)) + geom_tile(aes(fill = Frequency)) + scale_fill_gradient(low="white", high="blue")
 
-freq$age = as.numeric(freq$age)
-aggdata <-aggregate(freq, by=list(freq$elim_week), 
-                    FUN=mean, na.rm=TRUE)
-season_10 = subset(cont, cont$Season==10)
 
-age_elim = cont[, names(cont) %in% c("ethnicity", "ElimWeek")]
-a = aggregate(Ethnicity ~ ElimWeek, data=age_elim, mean)
+race_elim = cont[, names(cont) %in% c("ethnicity", "ElimWeek")]
+race_elim_agg = aggregate(Ethnicity ~ ElimWeek, data=age_elim, mean)
 
-race = as.matrix.data.frame(table(age_elim))
-names(race) = c("elim", "race", "freq")
-
-table(age_elim)
-
-cont = cont[, -60]
-ggplot(race, aes("elim", "race")) + 
-  geom_bar(stat="freq")
-
-age_elim$ethnicity = droplevels(ethnicity, exclude = if(anyNA(levels(x))) 
-  
-  
-ggplot(data=age_elim, aes(x=ethnicity)) + 
-  geom_bar(aes(y = (..count..)/sum(..count..)), fill="violet") + theme_minimal()
-
-myData = age_elim[age_elim$ethnicity != "Caucasian",]
+# clean up double caucasian entries
+myData = race_elim[race_elim$ethnicity != "Caucasian",]
 myData = myData[myData$ethnicity != "NA",]
-
-
 myData = subset(myData, myData$ElimWeek>8)
 race = as.matrix.data.frame(table(age_elim))
 names(race) = c("elim", "race", "freq")
-
-
 ggplot(data=myData, aes(x=ethnicity)) + 
   geom_bar(aes(y = (..count..)/sum(..count..)), fill="purple") + theme_minimal()
 
-
-corr = c()
-for (row in 1:nrow(season_10)) {
-  correlation_value = cor(t(season_10[row, -1:-2]), t(season_10_bach[,-1]))
-  corr = c(corr, correlation_value)
-}
-
-plot(corr, season_10$ElimWeek)
-
+############################# calc pearson correlation  ####################################
 
 calculate_correlation = function(season, bach, cont) {
   cont_subset = subset(cont, cont$Season==season)
@@ -204,6 +169,7 @@ calculate_correlation = function(season, bach, cont) {
   return(corr)
 }
 
+############################# calc euclidian distance  ####################################
 
 calc_distance = function(season, bach, cont) {
   cont_subset = subset(cont, cont$Season==season)
@@ -216,8 +182,8 @@ calc_distance = function(season, bach, cont) {
   return(dist)
 }
 
-barplot(table(elim_week))
-barplot(cont$Age, cont$ElimWeek)
+
+# do correlation and distance for seasons with data 
 
 elim_week = c()
 m_dist = c()
@@ -229,24 +195,26 @@ for(i in 10:22){
   m_dist = c(m_dist, calc_distance(i, bach, cont) )
   }
 
+# round to something reasonable for plotting 
 m_dist = round(m_dist, digits = 2)
 m_corr = round(m_corr, digits = 1)
 
+# make into a complete dataframe 
 df = do.call(rbind, Map(data.frame, A=elim_week, B=m_corr, C=m_dist))
 names(df) = c("elim", "corr", "dist")
 
-
-summary(lm(corr~dist, data=df))
-
+# linear models
+summary(lm(corr~dist, data=df)) 
 summary(lm(elim~corr, data=df))
 
+# box plot for looking at interactions 
 boxplot(dist~elim, data=df, col="red")
 ggplot(df, aes(elim, dist)) + 
   geom_boxplot()
 
+# frequency table for heatmap 
 d = as.data.frame(table(df$elim, df$corr))
 names(d) = c("elim", "corr", "freq")
 
-
 ggplot(d, aes(elim, corr)) + geom_tile(aes(fill = freq)) + scale_fill_gradient(low="white", high="red")
-}
+
